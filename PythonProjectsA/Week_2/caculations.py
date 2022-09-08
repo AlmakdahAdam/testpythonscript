4ca740d3117e3fd26fb4315845a2933aba9f78b9
AlmakdahAdam <112970819+AlmakdahAdam@users.noreply.github.com>
2022-09-08 13:13:16 -0400

    Add files via upload
    This is an HW assignment

---
+++ PythonProjectsA/Week_2/caculations.py
@@ -0,0 +1,35 @@
+#Addition
+my_addition = 3 + 5
+print(my_addition)
+
+#Substraction
+my_subtraction = 37-83
+print(my_subtraction)
+
+#Multiply
+my_multiply = 5*4567
+print(my_multiply)
+
+#Division
+my_division = 27 / 4
+print(my_division)
+
+
+number1 = int(input("Enter a number to check if it is divisible by 3: "))
+
+divisible_by_3 = number1 / 3
+print(divisible_by_3)
+
+#Modulo
+divis_by_3 = 27 % 3
+print(divis_by_3)
+
+#Exponents
+my_exponents = 3**4
+print(my_exponents)
+
+my_favorite_number = int(input("Enter your Favorite Number: "))
+print(f"Your favorite number is: {my_favorite_number}")
+
+my_favorite_number = int(input("Enter your Favorite Number: "))
+print(f"Your favorite number is: {my_favorite_number}")
